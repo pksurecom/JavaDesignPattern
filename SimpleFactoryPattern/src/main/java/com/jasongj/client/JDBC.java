deb7bad867f3b2f0672d7d6dea8dba3aff6765c6
Jason Guo <jason.guo.vip@gmail.com>
2016-04-02 09:45:28 +0800

    Add JDBC example for Simple Factory Pattern

---
+++ SimpleFactoryPattern/src/main/java/com/jasongj/client/JDBC.java
@@ -0,0 +1,36 @@
+package com.jasongj.client;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JDBC {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(JDBC.class);
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		try {
+			Class.forName("org.apache.hive.jdbc.HiveDriver");
+			conn = DriverManager.getConnection("jdbc:hive2://127.0.0.1:10000/default");
+			PreparedStatement ps = conn.prepareStatement("select count(*) from test.test");
+			ps.execute();
+		} catch (SQLException ex) {
+			LOG.warn("Execute query failed", ex);
+		} catch(ClassNotFoundException e) {
+			LOG.warn("Load Hive driver failed", e);
+		}finally{
+			if(conn != null ){
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// NO-OPT
+				}
+			}
+		}
+	}
+}
